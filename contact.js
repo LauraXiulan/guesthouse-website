5f8a8365773b1e7052a88a9bf4958c77ca9c7330
LauraXiulan <laurakramer91@gmail.com>
2016-10-07 18:13:55 +0200

    Added overlay on all pages, fixed banner and footer

--- contact.js
+++ contact.js
@@ -1,3 +1,14 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+
 $('#contact-form').bootstrapValidator({
 //        live: 'disabled',
 message: 'This value is not valid',
