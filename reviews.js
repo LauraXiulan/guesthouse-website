b4dcd879a5fcfffa9ac8e65f0ea9be388174177f
LauraXiulan <laurakramer91@gmail.com>
2016-10-07 18:13:55 +0200

    Added overlay on all pages, fixed banner and footer

--- reviews.js
+++ reviews.js
@@ -1,3 +1,14 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+
 var bar = new ProgressBar.Circle(container, {
   strokeWidth: 6,
   easing: 'easeInOut',
